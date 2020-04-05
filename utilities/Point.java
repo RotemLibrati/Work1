df0dfac6098616dff31da931f29c8a0352a28ecd
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-05 22:58:50 +0300

    Update functions in the whole project

--- utilities/Point.java
+++ utilities/Point.java
@@ -5,27 +5,29 @@ public class Point {
     private final double y;
 
     public Point(double x, double y) {
-        if (x>=0 && x<=1000000) this.x = x;
-        else this.x=0;
-        if (y>=0 && y<=800) this.y = y;
-        else this.y=0;
+        if (x >= 0 && x <= 1000000) this.x = x;
+        else this.x = 0;
+        if (y >= 0 && y <= 800) this.y = y;
+        else this.y = 0;
     }
+
     //getters
     public double getX() {
         return x;
     }
-    public double getY() { return y; }
 
-    public String toString(){
-        return String.valueOf('(' +   x + ',' + y + ')');
+    public double getY() {
+        return y;
     }
 
-    public boolean equals(Object o){
-        if (o==this)
-            return true;
-        if (!(o instanceof Point))
-            return false;
-        Point p = (Point)o;
-        return Double.compare(x,p.x) == 0 && Double.compare(y,p.y) == 0;
+    public String toString() {
+        return getClass().getName() + '@' + Integer.toHexString(hashCode());
+    }
+
+    public boolean equals(Object other) {
+        if (other instanceof Point) {
+            return x == ((Point) other).x && y == ((Point) other).y;
+        }
+        return false;
     }
 }
