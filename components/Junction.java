09a02f2b736436afdd3c3fd4b2d2649224fb442c
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-05 18:48:38 +0300

    update point

--- components/Junction.java
+++ components/Junction.java
@@ -8,4 +8,13 @@ public class Junction {
     private Point location; // location of the junction on the map
     private ArrayList<Road> enteringRoads;  // holds the list of the roads that enter to the junction.
     private ArrayList<Road> exitingRoads;   // holds the list of the roads that exit the junction.
+    private boolean hasLights;  // checks if the junction has traffic lights.
+    private int delay;  // delay time in seconds
+    private ArrayList<Road> vehicles;  //list of entering roads with cars waiting on the junction
+
+    public Junction(String name,Point loc){
+        junctionName=name;
+        location=new Point();
+
+    }
 }
