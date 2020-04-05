2fc18e2adcd67400e18b73fe6d3c70446d3a775e
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-05 18:48:38 +0300

    update point

--- utilities/Point.java
+++ utilities/Point.java
@@ -1,34 +1,30 @@
 package utilities;
 
 public class Point {
-
-   private final double x;
-    private final double y;
+    final double x;
+    final double y;
 
     public Point(double x, double y) {
-        if (x >= 0 && x <= 1000000) this.x = x;
-        else this.x = 0;
-        if (y >= 0 && y <= 800) this.y = y;
-        else this.y = 0;
-
+        if (x>=0 && x<=1000000) this.x = x;
+        else this.x=0;
+        if (y>=0 && y<=800) this.y = y;
+        else this.y=0;
     }
 
-    //getters
     public double getX() {
         return x;
     }
-
     public double getY() { return y; }
-
-
-    public String toString() {
-        return getClass().getName() + '@' + Integer.toHexString(hashCode());
+    public String toString(){
+        return String.valueOf('(' +   x + ',' + y + ')');
     }
 
-    public boolean equals(Object other) {
-        if (other instanceof Point) {
-            return x == ((Point) other).x && y == ((Point) other).y;
-        }
-        return false;
+    public boolean equals(Object o){
+        if (o==this)
+            return true;
+        if (!(o instanceof Point))
+            return false;
+        Point p = (Point)o;
+        return Double.compare(x,p.x) == 0 && Double.compare(y,p.y) == 0;
     }
 }
