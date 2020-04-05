028cff4472ac2930f42219162ac32da252051d1f
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-05 19:44:05 +0300

    Class junction

--- utilities/Point.java
+++ utilities/Point.java
@@ -1,8 +1,8 @@
 package utilities;
 
 public class Point {
-    final double x;
-    final double y;
+    private final double x;
+    private final double y;
 
     public Point(double x, double y) {
         if (x>=0 && x<=1000000) this.x = x;
@@ -10,11 +10,12 @@ public class Point {
         if (y>=0 && y<=800) this.y = y;
         else this.y=0;
     }
-
+    //getters
     public double getX() {
         return x;
     }
     public double getY() { return y; }
+
     public String toString(){
         return String.valueOf('(' +   x + ',' + y + ')');
     }
