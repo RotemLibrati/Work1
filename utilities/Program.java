5bb986b4f5e24f01f66b490771aecf2717cdb6a7
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-05 19:48:56 +0300

    program

---
+++ utilities/Program.java
@@ -0,0 +1,10 @@
+//Yehonatan Hen-207630112
+//Rotem Librati-
+
+package utilities;
+
+public class Program {
+    public static void main(String[] args) {
+        Point p = new Point(3,4);
+    }
+}
