327fa8ed9158184a7b8fca7c2224d63c376b5277
RotemLibrati <rotemlibrati1@gmail.com>
2020-04-06 10:58:49 +0300

    Route classs

---
+++ components/Route.java
@@ -0,0 +1,58 @@
+package components;
+
+import java.util.ArrayList;
+
+public class Route {
+    private ArrayList<Junction> junctions; //list of junctions on the
+    // route by the order of movement.
+    private ArrayList<Road> roads;// list of roads on the route by the
+    //order of movement.
+    private double delay; // time that will take vehicle to make this route.
+    private String vehicleType;
+
+    public Route(ArrayList<Junction> juncs, ArrayList<Road> roads, String vehType) {
+        this.junctions = juncs;
+        this.roads = roads;
+        this.vehicleType = vehType;
+        this.delay = 0;
+        this.vehicleType = null;
+    }
+    //not implemented in this task
+//    public Route(Junction start, Junction end, String vehType);
+//    {
+//
+//    }
+    //TODO : they dont write if we need getters and setters.. check and remove if we dont need.
+    //getters
+    public ArrayList<Junction> getJunctions() { return junctions;}
+    public ArrayList<Road> getRoads() { return roads;}
+    public double getDelay() { return delay; }
+    public String getVehicleType() { return vehicleType; }
+
+    //setters
+    public void setJunctions(ArrayList<Junction> junctions) { this.junctions = junctions; }
+    public void setRoads(ArrayList<Road> roads) { this.roads = roads; }
+    public void setDelay(double delay) { this.delay = delay; }
+    public void setVehicleType(String vehicleType) { this.vehicleType = vehicleType; }
+
+    //public Junction getStart() { return }
+    public double calcDelay() {
+        double length = 0;
+        int i = 0;
+        while(i < roads.size()) {
+            length += roads.get(i).getLength() / roads.get(i).getMaxSpeed();// the distance of the roads dividing max speed in roads.
+            i++;
+        }
+        while (i < junctions.size()) {
+            if (junctions.get(i).getHasLights()) {
+                length += junctions.get(i).getDelay() * (roads.size() - 1); //check if has traffic light in junction and calc time according formula .
+                i++;
+            }
+            //TODO: check how calc which road get priority and complete the else according to this.
+//            else {
+//
+//            }
+        }
+        return length;
+    }
+}
