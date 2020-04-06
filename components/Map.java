d7bbeeea8a253f43a34133d6fe0ea3df5592e78f
RotemLibrati <rotemlibrati1@gmail.com>
2020-04-06 16:25:40 +0300

    Map class, beginning

---
+++ components/Map.java
@@ -0,0 +1,13 @@
+package components;
+
+import java.util.ArrayList;
+
+public class Map {
+    private ArrayList<Junction> junctions;
+    private ArrayList<Road> roads;
+    public Map()// create a map with 20 random junctions and connect
+            //all of them one to another with roads.
+    {
+
+    }
+}
