4ec86e30786caa3d39f3505e4373b5e126c2455d
RotemLibrati <rotemlibrati1@gmail.com>
2020-04-10 00:53:34 +0300

    add JunctionTest and add random for type of vehicle and number of vehicle to VehicleType. i dont finished.

--- components/VehicleType.java
+++ components/VehicleType.java
@@ -2,9 +2,13 @@ package components;
 
 import utilities.Point;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class VehicleType {
     String typeName;
     int speed; // average speed of vehicle type
+
 
     public VehicleType(String name, int speed)
     {
@@ -29,5 +33,43 @@ public class VehicleType {
 
     public String getTypeName() {
         return typeName;
+    }
+
+    public ArrayList<VehicleType> getRandomVehicleTypes()
+    {
+        ArrayList<VehicleType> allowedVehicles;
+        allowedVehicles = new ArrayList<VehicleType>();
+        Random random = new Random();
+        int length = random.nextInt(7);
+        for( int i=0;i<length; i++) {
+            boolean flag=true;
+            Random rand = new Random();
+            int x = rand.nextInt(7);
+            if (x == 0 && flag == true) {
+                allowedVehicles.add(new VehicleType("Bicycle", 40));
+                flag=false;
+            } else if (x == 1 && flag == true) {
+                allowedVehicles.add(new VehicleType("Tram", 50));
+                flag = false;
+            }
+            if (x == 2 && flag == true) {
+                allowedVehicles.add(new VehicleType("Bus", 60));
+                flag=false;
+            } else if (x == 3 && flag == true){
+                allowedVehicles.add(new VehicleType("Car", 90));
+                flag=false;}
+            else if (x == 4 && flag == true) {
+                allowedVehicles.add(new VehicleType("Semitrailer", 80));
+                flag = false;
+            }
+            else if (x == 5 && flag == true) {
+                allowedVehicles.add(new VehicleType("Truck", 80));
+                flag = false;
+            }
+            else if (x == 6 && flag == true){
+                allowedVehicles.add(new VehicleType("MotorCycle", 120));
+                    flag=false;}
+        }
+        return allowedVehicles;
     }
 }
