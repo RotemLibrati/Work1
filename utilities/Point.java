c6ac1f39136727f02e19bc0aeecd3982f85aa6ac
RotemLibrati <rotemlibrati1@gmail.com>
2020-04-09 23:26:27 +0300

    fix Point class according to the example of Sofia and add TestPoint class for the commends.

--- utilities/Point.java
+++ utilities/Point.java
@@ -1,28 +1,71 @@
 package utilities;
 
+import java.util.Random;
+
 public class Point {
-    final double x;
-    final double y;
+    double x;
+    double y;
 
     public Point(double x, double y) {
+        boolean flag = true;
         if (x >= 0 && x <= 1000000) this.x = x;
-        else this.x = 0;
+        else
+        {
+            double newX =getRandomDoubleBetweenRange(1,1000000);
+            setX(newX);
+            System.out.println("The value " + x + " is illegal forX, therefore has been replaced with " + newX );
+            flag = false;
+
+        }
         if (y >= 0 && y <= 800) this.y = y;
-        else this.y = 0;
+        else
+        {
+            double newY =getRandomDoubleBetweenRange(1,800);
+            setY(newY);
+            System.out.println("The value " + y + " is illegal forY, therefore has been replaced with " + newY );
+            flag = false;
+        }
+        if(flag == true)
+            System.out.println("Point ("+ this.x + " , " + this.y + ") " + "has been created");
+        else {
+            Point n = new Point(this.x,this.y);
+        }
     }
     //getters
     public double getX() {
         return x;
     }
     public double getY() { return y; }
-    public String toString(){
-        return getClass().getName() + '@' + Integer.toHexString(hashCode());
+    //setters
+    public void setX(double x)
+    {
+        if(x >= 0 && x <= 1000000)
+            this.x = x;
+        else
+            System.out.println("The value " + x + " is illegal for x");
+    }
+    public void setY(double y)
+    {
+        if(y >= 0 && y <= 800)
+            this.y = y;
+        else
+            System.out.println("The value " + y + " is illegal for y");
     }
 
+    public String toString(){
+        return  "("+ getX() + " , "+ getY() + ")";
+    }
     public boolean equals(Object other) {
         if (other instanceof Point) {
             return x == ((Point) other).x && y == ((Point) other).y;
         }
         return false;
     }
+
+    public static double getRandomDoubleBetweenRange(double min, double max){
+        double x = (Math.random()*((max-min)+1))+min;
+        return x;
+    }
+
 }
+
