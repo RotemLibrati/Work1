837bddfc511df3c52b735f66061073f069f1ab3e
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-09 16:37:52 +0300

    Fix some bugs according to instructions... need to check how to make deep copy of object and implemet Map class

--- components/Map.java
+++ components/Map.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Map {
     private ArrayList<Junction> junctions;
     private ArrayList<Road> roads;
-    public Map()// create a map with 20 random junctions and connect
-            //all of them one to another with roads.
-    {
 
+
+    public Map(){
+        // create a map with 20 random junctions and connect
+        // all of them one to another with roads.
     }
 }
